0c7c66fa99f8c15a75df52785f5b59bfe4681637
Natalia <naticcg@gmail.com>
2020-03-13 18:45:21 -0300

    adding leaders section

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,4 +8,17 @@ $(document).ready(function() {
         navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
         checkVisible: false,
     });
+
+
+    $('.leaders-counter').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).data('value')
+        }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
 });
