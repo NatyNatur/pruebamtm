4d0450eb61ee15848e9f2716a35eb8f1bf1920af
Natalia <naticcg@gmail.com>
2020-03-13 23:27:18 -0300

    adding roles with modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,4 +21,19 @@ $(document).ready(function() {
             }
         });
     });
+
+    $('#rolModal').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var rol_html = button.prev('h2').html();
+        var img = button.prevAll('img').first().attr('src');
+        console.log(img);
+        var rol_sub = button.data('sub');
+        var rol_descr = button.data('descr');
+
+        var modal = $(this)
+        modal.find('.modal-title').text(rol_html);
+        modal.find('.modal-body .modal-img').attr('src', img);
+        modal.find('.modal-body .modal-rol-descr').text(rol_descr);
+        modal.find('.modal-body .modal-rol-sub').text(rol_sub);
+    })
 });
