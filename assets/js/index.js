b26fa2367ddd899fee9cd823c68320a54702935e
NatyNatur <naticcg@gmail.com>
2020-03-15 00:48:47 -0300

    some mq changes, adding fx, dinamic modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+var modal;
+
+
 $(document).ready(function() {
     $(".owl-carousel").owlCarousel({
         items: 1,
@@ -7,8 +10,26 @@ $(document).ready(function() {
         nav: true,
         navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
         checkVisible: false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:true,
+                loop:false
+            }
+        }
     });
 
+    $('.closeMoreInfo').on('click', function() {
+        $('div.moreInfo').hide();
+    })
 
     $('.leaders-counter').each(function() {
         $(this).prop('Counter', 0).animate({
@@ -24,16 +45,21 @@ $(document).ready(function() {
 
     $('#rolModal').on('show.bs.modal', function(event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
-        var rol_html = button.prev('h2').html();
+        //var rol_html = button.prev('h2').html();
         var img = button.prevAll('img').first().attr('src');
         console.log(img);
+        var rol_name = button.data('rolname');
         var rol_sub = button.data('sub');
         var rol_descr = button.data('descr');
+        var rol_color = button.data('color');
 
-        var modal = $(this)
-        modal.find('.modal-title').text(rol_html);
+        modal = $(this);
+        modal.find('.modal-header').addClass('rol-' + rol_color);
+        modal.find('.modal-title').text(rol_name);
         modal.find('.modal-body .modal-img').attr('src', img);
         modal.find('.modal-body .modal-rol-descr').text(rol_descr);
-        modal.find('.modal-body .modal-rol-sub').text(rol_sub);
+        modal.find('.modal-body .modal-rol-sub').text(rol_sub).addClass(rol_color + 'Font');
     })
+
+    // remover clases de colores al cerrar modal.
 });
